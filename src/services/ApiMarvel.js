a70f8986375ac8e0d1d3bb08001eeb04898a468c
Givanildo <givanildo.altaneira@gmail.com>
2021-05-06 22:23:17 -0300

    Correção de erro do carregamento dos comics https-api

--- src/services/ApiMarvel.js
+++ src/services/ApiMarvel.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const APIMarvel = axios.create({
-    baseURL: 'http://gateway.marvel.com/'
+    baseURL: 'https://gateway.marvel.com/'
 });
 
 export default APIMarvel;
